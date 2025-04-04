3392dfe9bd3c3feb312959d18496a28b3b10de58
kavehkm <mehrbaniankaveh@gmail.com>
2025-04-04 16:41:10 +0330

    update DJSMSConf: 1) add new @use_django_q 2) remove @django_q_installed

--- djsms/conf.py
+++ djsms/conf.py
@@ -1,13 +1,21 @@
+# standard
+from typing import Any
+
 # dj
 from django.conf import settings
+from django.utils.functional import cached_property
 
 
 class DJSMSConf(object):
     """DJSMS Conf"""
 
-    @property
-    def django_q_installed(self):
-        return "django_q" in settings.INSTALLED_APPS
+    @staticmethod
+    def get_settings(key, default=None) -> Any:
+        return getattr(settings, key, default)
+
+    @cached_property
+    def use_django_q(self):
+        return self.get_settings("DJSMS_USE_DJANGO_Q", True)
 
 
 djsms_conf = DJSMSConf()

060cfb6e7d16147ec9a82981bbf7140ed1e60bf6
kavehkm <mehrbaniankaveh@gmail.com>
2025-04-04 16:30:01 +0330

    update BaseBackend class: 1) replace Message with dict as return type of send_* methods 2) remove get_status() method

--- djsms/backends/base.py
+++ djsms/backends/base.py
@@ -1,6 +1,9 @@
 # standard
 from typing import Any, List
 
+# internal
+from ..models import Message
+
 
 class BaseBackend(object):
     """Base Backend"""
@@ -20,10 +23,10 @@ class BaseBackend(object):
     def _get_config(self, name: str, default: Any = None) -> Any:
         return self._config.get(name, default)
 
-    def send(self, text: str, to: str, **kwargs: Any) -> dict:
+    def send(self, text: str, to: str, **kwargs: Any) -> Message:
         raise NotImplementedError
 
-    def send_bulk(self, text: str, to: List[str], **kwargs: Any) -> dict:
+    def send_bulk(self, text: str, to: List[str], **kwargs: Any) -> Message:
         raise NotImplementedError
 
     def send_schedule(
@@ -36,23 +39,20 @@ class BaseBackend(object):
         hours: int,
         minutes: int,
         **kwargs: Any,
-    ) -> dict:
+    ) -> Message:
         raise NotImplementedError
 
     def send_pattern(
         self, pattern_id: int, to: str, args: List[str], **kwargs: Any
-    ) -> dict:
+    ) -> Message:
         raise NotImplementedError
 
     def send_multiple(
         self, texts: List[str], recipients: List[str], **kwargs: Any
-    ) -> dict:
+    ) -> Message:
         raise NotImplementedError
 
     def get_credit(self) -> int:
-        raise NotImplementedError
-
-    def get_status(self, ids: List[int]) -> dict:
         raise NotImplementedError
 
     def __str__(self):
